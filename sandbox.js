c16ab09586b966f7c74a0730e1bf55188d0a2c68
Rimah Bhabha <rbhabha@academic.rrc.ca>
2024-01-05 09:51:29 -0600

    completed fourth training video

--- sandbox.js
+++ sandbox.js
@@ -341,3 +341,123 @@ if(true){
 console.log('outside code block: ', age, name, test); */
 
 // VIDEO 4 -- Functions & Methods
+
+// function expression
+/* const speak =  function(){
+  console.log('good day');
+}; */
+
+//greet();
+//greet();
+//speak();
+//speak();
+//greet();
+
+// function declaration
+/* function greet(){
+  console.log('hello there');
+} */
+
+// arguments & parameters
+/* const speak =  function(name = 'luigi', time = 'night'){
+  console.log(`good ${time} ${name}`);
+}; */
+
+//speak('mario', 'morning');
+//speak();
+//speak('shaun');
+
+// returning values
+
+/* const calcArea = function(radius){
+  return 3.14 * radius**2;
+}; */
+
+
+
+// arrow functions
+
+/* const calcArea = radius => 3.14 * radius**2;
+
+
+const area = calcArea(5);
+console.log('area is:', area); */
+
+/* const greet = function(){
+  return 'hello, world';
+}; */
+
+/* const greet = () => 'hello, world';
+const result = greet();
+console.log(result); */
+
+/* const bill = function(products, tax){
+  let total = 0;
+  for(let i = 0; i < products.length; i++){
+    total += products[i] + products [i] * tax;
+  }
+  return total;
+} */
+
+/* const bill = (products, tax) => {
+  let total = 0;
+  for(let i = 0; i < products.length; i++){
+    total += products[i] + products [i] * tax;
+  }
+  return total;
+};
+
+console.log(bill([10, 15, 30], 0.2)); */
+
+//const name = 'shaun';
+
+// functions
+
+//const greet = () => 'hello';
+
+//let resultOne = greet();
+//console.log(resultOne);
+
+// methods
+
+//let resultTwo = name.toUpperCase();
+//console.log(resultTwo);
+
+// callbacks & foreach
+
+/* const myFunc = (callbackFunc) => {
+  let value = 50;
+  callbackFunc(value);
+};
+
+myFunc(value => {
+  console.log(value);
+}); */
+
+/* let people = ['mario', 'luigi', 'ryu', 'shaun', 'chun-li'];
+
+const logPerson = (person, index) => {
+  console.log(`${index} - hello ${person}`);
+}; */
+
+/* people.forEach((person, index) => {
+  console.log(index, person);
+}); */
+
+//people.forEach(logPerson);
+
+// get a reference to the 'ul'
+/* const ul = document.querySelector('.people');
+
+const people = ['mario', 'luigi', 'ryu', 'shaun', 'chun-li'];
+
+let html = ``;
+
+people.forEach(person => {
+  // create html template
+  html += `<li style="color: purple">${person}</li>`;
+});
+
+console.log(html);
+ul.innerHTML = html; */
+
