44159ec2e7ef6b646a62857e18463d9a24a6b446
Rimah Bhabha <rbhabha@academic.rrc.ca>
2024-01-05 10:55:36 -0600

    completed fifth training video

--- sandbox.js
+++ sandbox.js
@@ -461,3 +461,97 @@ people.forEach(person => {
 console.log(html);
 ul.innerHTML = html; */
 
+// VIDEO 5 -- Object Literals
+
+/* const blogs = [
+  { title: 'why mac & cheese rules', likes: 30 },
+  { title: '10 things to make with marmite', likes: 50 }
+];
+
+console.log(blogs); */
+
+/* let user = {
+  name: 'crystal',
+  age: 30,
+  email: 'crystal@thenetninja.co.uk',
+  location: 'berlin',
+  blogs: [
+    { title: 'why mac & cheese rules', likes: 30 },
+    { title: '10 things to make with marmite', likes: 50 }
+  ],
+  login(){
+    console.log('the user logged in');
+  },
+  logout(){
+    console.log('the user logged out');
+  },
+  logBlogs(){
+    //console.log(this.blogs);
+    console.log('this user has written the following blogs:');
+    this.blogs.forEach(blog => {
+      console.log(blog.title, blog.likes);
+    })
+  }
+};
+
+user.logBlogs(); */
+
+//user.logBlogs();
+//console.log(this);
+
+/* console.log(user);
+console.log(user.name);
+//user.age = 35;
+console.log(user.age);
+
+const key = 'location';
+console.log(user['name']);
+console.log(user['email']);
+user['name'] = 'chun-li';
+console.log(user['name']);
+console.log(user[key]);
+
+console.log(typeof user); */
+
+//user.login();
+//user.logout();
+
+// Math objects
+
+/* console.log(Math);
+console.log(Math.PI);
+console.log(Math.E);
+
+const area = 7.7;
+
+console.log(Math.round(area));
+console.log(Math.floor(area));
+console.log(Math.ceil(area));
+console.log(Math.trunc(area));
+
+// random numbers
+
+const random = Math.random();
+
+console.log(random);
+console.log(Math.round(random * 100)); */
+
+// primitive values
+
+/* let scoreOne = 50;
+let scoreTwo = scoreOne;
+
+console.log(`scoreOne: ${scoreOne}`, `scoreTwo: ${scoreTwo}`);
+scoreOne = 100;
+console.log(`scoreOne: ${scoreOne}`, `scoreTwo: ${scoreTwo}`); */
+
+// reference values
+
+/* const userOne = { name: 'ryu', age: 30 };
+const userTwo = userOne;
+
+console.log(userOne, userTwo);
+
+userOne.name = 'chun-li';
+console.log(userOne, userTwo); */
+
