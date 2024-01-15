98fc7688c33583c669e708d77fc1fcad9905001a
Rimah Bhabha <rbhabha@academic.rrc.ca>
2024-01-15 11:04:56 -0600

    completed section 7 training videos

--- chapter 7/sandbox.js
+++ chapter 7/sandbox.js
@@ -1,8 +1,49 @@
 const form = document.querySelector('.signup-form');
-const username = document.querySelector('#username');
+//const username = document.querySelector('#username');
+const feedback = document.querySelector('.feedback');
+const usernamePattern = /^[a-zA-Z]{6,12}$/;
 
 form.addEventListener('submit', e => {
   e.preventDefault();
   //console.log(username.value);
-  console.log(form.username.value);
+  //console.log(form.username.value);
+
+  // validation
+  //const username = form.username.value;
+  const usernamePattern = /^[a-zA-Z]{6,12}$/;
+
+  if(usernamePattern.test(username)){
+    // feedback good info
+    feedback.textContent = 'that username is valid!';
+  } else {
+    // feedback help info
+    feedback.textContent = 'username must contain letters only & be between 6-12 characters long';
+
+  }
 });
+
+// live feedback
+form.username.addEventListener('keyup', e => {
+  console.log(e);
+  //console.log(e.target.value, form.username.value);
+  if(usernamePattern.test(e.target.value)){
+    form.username.setAttribute('class', 'success');
+  } else {
+    form.username.setAttribute('class', 'error');
+  }
+});
+
+// testing RegEx
+//const username = 'shaunp';
+//const pattern = /^[a-z]{6,}$/;
+
+/* let result = pattern.test(username);
+
+if(result){
+  console.log('regex test passed :)');
+} else {
+  console.log('regex test failed :(');
+}; */
+
+//let result = username.search(pattern);
+//console.log(result);
